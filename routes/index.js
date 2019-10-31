d4061b640975976fecead36f25e81f3e6e97c7a4
Andres Fernando <andres.fernando08@gmail.com>
2019-10-31 00:24:39 -0500

    agregada una plantilla jade y se enlazó al router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router    = require('express').Router()
 
-router.get('/', (req, res) => res.send('Migraciones'))
+router.get('/', (req, res) => res.render('index'))
 
 
 module.exports = router
